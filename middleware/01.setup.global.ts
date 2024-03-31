e7f3a739eb2a64c98612692b232a4d9f71972fb0
Denys Petryniak <denys.petryniak@gmail.com>
2024-03-31 04:11:25 +0300

    Add example of error handling in middleware

--- middleware/01.setup.global.ts
+++ middleware/01.setup.global.ts
@@ -1,3 +1,19 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   console.log("--- Setup Middleware ---");
+
+  if (to.fullPath === "/login?test=true") {
+    throw createError({
+      statusCode: 405,
+      statusMessage: "Login not allowed",
+    });
+  }
+
+  // return abortNavigation("Failed at global setup middleware");
+
+  // try {
+  //   const data = await $fetch("/api/test-path");
+  //   return navigateTo(`/profile/${data.username}`);
+  // } catch (error) {
+  //   abortNavigation(error);
+  // }
 });
